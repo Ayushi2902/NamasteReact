9cffa1cf8d8620d6ca669427b813cf779eef9f4a
Ayushi2902 <ayushisingh.email@gmail.com>
2026-01-23 19:58:08 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,28 @@
+/*const heading = React.createElement(
+  "h1",
+  { id: "title", key: "h1" },
+  "Hello from React",
+);
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "h1" }, "I am h1 tag"),
+    React.createElement("h2", { key: "h2" }, "I am h2 tag"),
+  ],React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { key: "h1" }, "I am h1 tag"),
+    React.createElement("h2", { key: "h2" }, "I am h2 tag"),
+  ]))
+);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); //putting the parent element in the root
+
+
